b73ecd01b1faefd787e628fa0f693038ff200b78
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-21 23:30:14 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,9 @@
 def find_max_value(array)
-  
+  i = 1
+  max_value = array[0]
+  while i < array.length do
+    if(max_value < array[i])
+      max_value = array[i]
+    end
+    return max_value
 end
